e4804d18e501ba1738f96c2ba0ffef28171159f4
RMP <rafelmayol61@gmail.com>
2024-02-22 13:32:33 +0100

    Lecion 8 gas avanzado

---
+++ funciones.js
@@ -0,0 +1,10 @@
+function doGet()
+{
+    return HtmlService.createTemplateFromFile('web').evaluate().setTitle('Agenda GAS');
+}
+
+function obtenerDatosHtml(nombre)
+{
+    return HtmlService.createHtmlOutputFromFile(nombre).getContent();
+
+}
