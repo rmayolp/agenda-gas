22f6d2c937760932a0e5abd8c9adf1a55dc400d8
RMP <rafelmayol61@gmail.com>
2024-02-22 21:47:06 +0100

    Leccion 9 GAS Avanzado

--- funciones.js
+++ funciones.js
@@ -8,3 +8,13 @@ function obtenerDatosHtml(nombre)
     return HtmlService.createHtmlOutputFromFile(nombre).getContent();
 
 }
+
+function obtenerContactos()
+{
+    let ss = SpreadsheetApp.openById('1w-U_y-_IVWcIW2e2kFAAOi2_b6KFwrrR1surjZuguSo');
+    let hoja = ss.getActiveSheet();
+    let datos = hoja.getDataRange().getValues();
+
+    return datos;
+
+}
