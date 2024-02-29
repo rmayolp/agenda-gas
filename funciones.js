5b0d48c971bdf21fd8e851dfd211a797e356d3bf
RMP <rafelmayol61@gmail.com>
2024-02-29 22:34:55 +0100

    Lliço 15 - GAS avançat

--- funciones.js
+++ funciones.js
@@ -1,20 +1,33 @@
-function doGet()
+
+const HOJA = SpreadsheetApp.openById('1w-U_y-_IVWcIW2e2kFAAOi2_b6KFwrrR1surjZuguSo').getActiveSheet();
+
+function doGet(datos)
 {
+    insertarContacto( datos.parameter.nombre, datos.parameter.correo ); 
     return HtmlService.createTemplateFromFile('web').evaluate().setTitle('Agenda GAS');
 }
+
+// Recibir el intro del formulario (no se ven en la url)
+function doPost(datos)
+{
+    //Accedemos al campo del formulario cuyo "name" es nombre
+    insertarContacto( datos.parameter.nombre, datos.parameter.correo ); 
+    return HtmlService.createTemplateFromFile('web').evaluate().setTitle('Agenda GAS');
+}
+
+
 
 function obtenerDatosHtml(nombre)
 {
     return HtmlService.createHtmlOutputFromFile(nombre).getContent();
-
 }
 
 function obtenerContactos()
 {
-    let ss = SpreadsheetApp.openById('1w-U_y-_IVWcIW2e2kFAAOi2_b6KFwrrR1surjZuguSo');
-    let hoja = ss.getActiveSheet();
-    let datos = hoja.getDataRange().getValues();
+    return HOJA.getDataRange().getValues();
+}
 
-    return datos;
-
+function insertarContacto( nombre, correo)
+{
+    HOJA.appendRow([nombre,correo]);
 }
